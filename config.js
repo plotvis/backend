6cfde1398400f50a2fa16c8e433bd6d2562e811e
Cole Johnson <colehjohn@gmail.com>
2017-11-07 10:51:48 -0500

    Changed to mlab

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     port: process.env.PORT || 8080,
-    mongodbUrl: 'mongodb://admin:Plotvissecurepass@cluster0-shard-00-00-rjote.mongodb.net:27017,cluster0-shard-00-01-rjote.mongodb.net:27017,cluster0-shard-00-02-rjote.mongodb.net:27017/plotvis?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin' || process.env.mongoUrl || 'mongodb://localhost:27017/plotvis',
-    // mongodbUrl:  process.env.mongoUrl || 'mongodb://localhost:27017/plotvis',
+    mongodbUrl:  'mongodb://plotvis:Plotvissecurepass@ds251245.mlab.com:51245/plotvis' || process.env.mongoUrl || 'mongodb://localhost:27017/plotvis',
     secret: 'ThIsIsSoSeCrEt!!',
     rootDir: __dirname
 };
